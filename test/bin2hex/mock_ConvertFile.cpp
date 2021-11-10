0f1460eb43d0c7de5296bd0fbdf60c44825cf292
Perry Anderson <perry.anderson@gmail.com>
2021-11-09 19:13:53 -0800

    Added mock

--- test/bin2hex/mock_ConvertFile.cpp
+++ test/bin2hex/mock_ConvertFile.cpp
@@ -1,6 +1,7 @@
 
 #include <rsi/bin2hex/ConvertFile.hpp>
 #include <iostream>
+#include <fstream>
 
 #include "../vendor/catch.hpp"
 #include "../vendor/fakeit.hpp"
@@ -10,22 +11,45 @@ using namespace fakeit;
 
 SCENARIO("Mock ConvertFileInterface", "[ConvertInterface]") {
 
-    rsi::HexFile hexFile;
-    rsi::BinFile binFile;
     Mock<rsi::ConvertFileInterface> mock;
     When(Method(mock, hexToBin))
         .AlwaysDo(
-            [&binFile](const rsi::HexFile& hexFile) {
-
+            [](const rsi::HexFile& hexFile) {
+                rsi::BinFile binFile;
+                rsi::ConvertLine converter;
+                for (auto hexLine : hexFile) {
+                    rsi::BinLine binline = converter.hexToBin(hexLine);
+                    binFile.push_back(binline);
+                }
                 return binFile;
             });
     When(Method(mock, binToHex))
         .AlwaysDo(
-            [&hexFile](const rsi::BinFile& binFile) {
+            [](const rsi::BinFile& binFile) {
+                rsi::HexFile hexFile;
+                rsi::ConvertLine converter;
+                for (auto binLine : binFile) {
+                    rsi::HexLine hexline = converter.binToHex(binLine);
+                    hexFile.push_back(hexline);
+                }
                 return hexFile;
             });
 
+    rsi::HexFile hexFile;
+    rsi::BinFile binFile;
+
+    std::ifstream in("send.txt");
+    REQUIRE(in.good());
+    while (in.good()) {
+        std::string line;
+        getline(in, line);
+        if (in.good())
+            hexFile.push_back(line);
+    }
+
     rsi::ConvertFileInterface& i = mock.get();
+    binFile = i.hexToBin(hexFile);
+    hexFile = i.binToHex(binFile);
     REQUIRE(i.hexToBin(hexFile) == binFile);
     REQUIRE(i.binToHex(binFile) == hexFile);
     Verify(Method(mock, hexToBin));
