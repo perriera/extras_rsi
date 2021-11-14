572416da9156dab71738cc8fe781a511291c6415
Perry Anderson <perry.anderson@gmail.com>
2021-11-13 19:50:25 -0800

    Work in progress

--- include/rsi/vendor/v2/Vendor.hpp
+++ include/rsi/vendor/v2/Vendor.hpp
@@ -1,90 +1,125 @@
-// #ifndef _EXPARX_RSISERVICES_VENDER_HPP
-// #define _EXPARX_RSISERVICES_VENDER_HPP
+#ifndef _EXPARX_RSISERVICES_VENDER_HPP
+#define _EXPARX_RSISERVICES_VENDER_HPP
 
-// #include <extras/interfaces.hpp>
-// #include <rsi/sockets/Types.hpp>
-// #include <rsi/uploader/Uploader.hpp>
-// #include <rsi/parcel/Parcel.hpp>
-// #include <iostream>
-// #include <sstream>
-// #include <netinet/in.h>
-// #include <sys/socket.h>
+#include <extras/interfaces.hpp>
+#include <rsi/sockets/Types.hpp>
+#include <rsi/uploader/Uploader.hpp>
+#include <iostream>
+#include <sstream>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-// namespace extras {
-//     namespace rsi {
-//         /**
-//          * @brief VendorInterface
-//          * @note at this point the file in question to be operated on has
-//          * now been successfully uploaded to the server and it is up to
-//          * the vendor to do something withit. As soon as this method
-//          * completes the file specifed under filename is considered
-//          * changed and will be automatically downloaded back to the client.
-//          * For simplicity, the same filename will be used. This makes it
-//          * easier for the support system to remove the file as soon as it
-//          * has been successfully downloaded back to the client.
-//          *
-//          */
+namespace extras {
+    namespace rsi {
+        /**
+         * @brief VendorInterface
+         * @note at this point the file in question to be operated on has
+         * now been successfully uploaded to the server and it is up to
+         * the vendor to do something withit. As soon as this method
+         * completes the file specifed under filename is considered
+         * changed and will be automatically downloaded back to the client.
+         * For simplicity, the same filename will be used. This makes it
+         * easier for the support system to remove the file as soon as it
+         * has been successfully downloaded back to the client.
+         *
+         */
 
-//         abstract class Vendor implements rsi::UploaderInterface {
+        interface VendorInterface {
 
-//         protected:
+            /**
+             * @brief add_value()
+             * @param filename
+             */
+            virtual Parameter parcel() const pure;
 
-//             rsi::UploaderInterface& _uploader;
-//             rsi::Parcel _parcel;
+            virtual void wrapParcel() pure;
+            virtual void deliverParcel() pure;
+            virtual void unwrapParcel() pure;
 
-//         public:
+        };
 
-//             Vendor(rsi::UploaderInterface& uploader) : _uploader(uploader) {}
+        /**
+         * @brief Vendor
+         *
+         */
 
-//             virtual Parameters parameters(int argc, char const* argv[]) override {
-//                 return _uploader.parameters(argc, argv);
-//             };
-//             virtual const Parameter& program() const override;
-//             virtual const Parameter& payload() const override;
-//             virtual const Parameter& ip() const override;
-//             virtual const Parameter& port() const override;
-//             virtual Parameter parcel() const override;
+        abstract class Vendor implements VendorInterface with UploaderInterface {
 
-//         };
+        protected:
 
-//         /**
-//         * @brief VendorClient
-//         *
-//         */
-//         concrete class VendorClient extends Vendor {
+            rsi::UploaderInterface& _proxy;
 
-//             rsi::UploaderClient _client;
+            Vendor(rsi::UploaderInterface& proxy) :_proxy(proxy) {}
 
-//         public:
+        public:
 
-//             VendorClient() :Vendor(_client) {}
+            /**
+             * @brief add_value()
+             * @param filename
+             */
+            virtual void wrapParcel() override;
+            virtual void deliverParcel() override;
+            virtual void unwrapParcel() override;
 
-//             virtual void connect() override;
-//             virtual void transfer() override;
-//             virtual void close() override;
+            virtual Parameters parameters(int argc, char const* argv[]) override {
+                return _proxy.parameters(argc, argv);
+            }
+            virtual const Parameter& program() const override {
+                return _proxy.program();
+            }
+            virtual const Parameter& payload() const override {
+                return _proxy.payload();
+            }
+            virtual Parameter parcel() const override {
+                return  _proxy.parcel();
+            }
+            virtual const Parameter& ip() const override {
+                return _proxy.ip();
+            }
+            virtual const Parameter& port() const override {
+                return _proxy.port();
+            }
 
-//         };
+        };
 
-//         /**
-//          * @brief VendorServer
-//          *
-//          */
-//         concrete class VendorServer extends Vendor {
+        /**
+         * @brief VendorClient
+         *
+         */
+        concrete class VendorClient extends Vendor {
 
-//             rsi::UploaderServer _server;
+            rsi::UploaderClient _client;
 
-//         public:
+        public:
 
-//             VendorServer() :Vendor(_server) {}
+            VendorClient() :Vendor(_client) {}
 
-//             virtual void connect() override;
-//             virtual void transfer() override;
-//             virtual void close() override;
+            virtual void connect() override;
+            virtual void transfer() override;
+            virtual void close() override;
 
-//         };
+        };
 
-//     }  // namespace rsi
+        /**
+         * @brief VendorServer
+         *
+         */
+        concrete class VendorServer extends Vendor {
 
-// }  // namespace extras
+            rsi::UploaderServer _server;
 
-// #endif  // _EXPARX_RSISERVICES_VENDER_HPP
+        public:
+
+            VendorServer() :Vendor(_server) {}
+
+            virtual void connect() override;
+            virtual void transfer() override;
+            virtual void close() override;
+
+        };
+
+    }  // namespace rsi
+
+}  // namespace extras
+
+#endif  // _EXPARX_RSISERVICES_VENDER_HPP
--- test/vendor/v2/test_Vendor.cpp
+++ test/vendor/v2/test_Vendor.cpp
@@ -1,45 +1,45 @@
-// #include <rsi/vendor/v2/Vendor.hpp>
-// #include <extras/strings.hpp>
-// #include <extras/filesystem/paths.hpp>
-// #include <filesystem>
-// #include <iostream>
+#include <rsi/vendor/v2/Vendor.hpp>
+#include <extras/strings.hpp>
+#include <extras/filesystem/paths.hpp>
+#include <filesystem>
+#include <iostream>
 
-// #include "../../unittesting/catch.hpp"
-// #include "../../unittesting/fakeit.hpp"
+#include "../../unittesting/catch.hpp"
+#include "../../unittesting/fakeit.hpp"
 
-// using namespace extras;
-// namespace fs = std::filesystem;
+using namespace extras;
+namespace fs = std::filesystem;
 
-// SCENARIO("Test VendorInterface (v2)", "[VendorInterface]") {
+SCENARIO("Test VendorInterface (v2)", "[VendorInterface2]") {
 
-//     rsi::Parameter parcel = ~extras::Paths("data/cplusplusorg.freeformjs.imploded.zip");
-//     const char* argv[] = {
-//         "socketclient", parcel.c_str(),  "127.0.0.1", "8080",
-//         "vendor",   "download" };
-//     int argc = 6;
+    rsi::Parameter parcel = ~extras::Paths("data/cplusplusorg.freeformjs.imploded.zip");
+    const char* argv[] = {
+        "socketclient", parcel.c_str(),  "127.0.0.1", "8080",
+        "vendor",   "download" };
+    int argc = 6;
 
-//     rsi::VendorClient testUnit;
-//     testUnit.parameters(argc, argv);
-//     rsi::VendorInterface& i = testUnit;
+    rsi::VendorClient testUnit;
+    testUnit.parameters(argc, argv);
+    rsi::VendorInterface& i = testUnit;
 
-//     // // wrap parcel
-//     // if (fs::exists(testUnit.parcel()))
-//     //     fs::remove(testUnit.parcel());
-//     // REQUIRE(!fs::exists(testUnit.parcel()));
-//     // REQUIRE(fs::exists(testUnit.payload()));
-//     // i.wrapParcel();
-//     // REQUIRE(fs::exists(testUnit.parcel()));
+    // wrap parcel
+    if (fs::exists(testUnit.parcel()))
+        fs::remove(testUnit.parcel());
+    REQUIRE(!fs::exists(testUnit.parcel()));
+    REQUIRE(fs::exists(testUnit.payload()));
+    i.wrapParcel();
+    REQUIRE(fs::exists(testUnit.parcel()));
 
-//     // // deliver parcel
-//     // REQUIRE(fs::exists(testUnit.parcel()));
-//     // fs::remove(testUnit.payload());
-//     // REQUIRE(!fs::exists(testUnit.payload()));
-//     // i.deliverParcel();
-//     // REQUIRE(fs::exists(testUnit.payload()));
-//     // fs::remove(testUnit.payload());
-//     // REQUIRE(!fs::exists(testUnit.payload()));
+    // deliver parcel
+    REQUIRE(fs::exists(testUnit.parcel()));
+    fs::remove(testUnit.payload());
+    REQUIRE(!fs::exists(testUnit.payload()));
+    i.deliverParcel();
+    REQUIRE(fs::exists(testUnit.payload()));
+    fs::remove(testUnit.payload());
+    REQUIRE(!fs::exists(testUnit.payload()));
 
-//     // // un parcel
-//     // i.unwrapParcel();
-//     // REQUIRE(fs::exists(testUnit.payload()));
-// }
+    // un parcel
+    i.unwrapParcel();
+    REQUIRE(fs::exists(testUnit.payload()));
+}
