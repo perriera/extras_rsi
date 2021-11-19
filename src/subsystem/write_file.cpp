7dc04453cc0fe039a0af352d6052e4c77c5a67ea
Perry Anderson <perry.anderson@gmail.com>
2021-11-18 23:31:51 -0800

    Updated

--- src/subsystem/write_file.cpp
+++ src/subsystem/write_file.cpp
@@ -5,7 +5,9 @@
 #include <unistd.h>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <rsi/sockets/StatusBar.hpp>
+#include <rsi/parcel/Line.hpp>
 #include <rsi/subsystem.hpp>
 #include <extras/strings.hpp>
 
@@ -14,7 +16,6 @@ using namespace std;
 void extras::rsi::write_file(const std::string& filename, int sockfd) {
 
     int count = 0;
-    int max = StatusBar::lastLinesInFile;
     int n;
     char buffer[extras::rsi::SIZE];
 
@@ -23,12 +24,26 @@ void extras::rsi::write_file(const std::string& filename, int sockfd) {
     while (true) {
         n = recv(sockfd, buffer, extras::rsi::SIZE, 0);
 
-
+        //
+        //
         std::string msg(buffer);
-
         auto parts = extras::split(msg, '\n');
         count += parts.size();
-        std::cout << bar(count, max);
+        stringstream ss;
+        ss << msg;
+        string line, line2;
+        getline(ss, line);
+        getline(ss, line);
+        getline(ss, line2);
+        if (line2.size() > 0) {
+            stringstream ss2;
+            ss2 << line;
+            PackedLine packed;
+            ss2 >> packed;
+            std::cout << bar(count, packed.maxCount());
+        }
+        //
+        //
 
         if (extras::contains(msg, "done")) {
             msg = extras::replace_all(msg, "done", "");
