4add3d7fa7f43941d37ef7e1cf5b5b1c86201e49
Perry Anderson <perry.anderson@gmail.com>
2021-11-18 18:21:36 -0800

    Updated

--- src/sockets/Status.cpp
+++ src/sockets/Status.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 
 #include <rsi/sockets/Status.hpp>
+#include <extras/devices/ansi_colors.hpp>
 #include <rsi/exceptions.hpp>
 #include <extras/strings.hpp>
 #include <iostream>
@@ -9,26 +10,47 @@
 using namespace std;
 
 namespace extras {
+
+
     namespace rsi {
+
+
+        std::string right() {
+            stringstream ss;
+            ss << extras::green << '[';
+            return ss.str();
+        }
+
+        std::string left() {
+            stringstream ss;
+            ss << extras::green << ']';
+            return ss.str();
+        }
+
+        std::string status(char c) {
+            stringstream ss;
+            ss << right() << extras::yellow << c << left();
+            return ss.str();
+        }
 
         StatusMsg StatusLine::start(const StatusMsg& msg) const {
             auto parts = extras::split(msg, '/');
             std::stringstream ss;
-            ss << "[+] " << parts[parts.size() - 1] << " started ";
+            ss << status('+') << extras::cyan << " " << parts[parts.size() - 1] << " started " << extras::reset;
             return ss.str();
 
         }
 
         StatusMsg StatusLine::pass(const StatusMsg& msg) const {
             std::stringstream ss;
-            ss << "[+] " << msg << ".";
+            ss << status('+') << extras::magenta << " " << msg << "." << extras::reset;
             return ss.str();
 
         }
 
         StatusMsg StatusLine::fail(const StatusMsg& msg) const {
             std::stringstream ss;
-            ss << "[+] " << msg << ".";
+            ss << status('-') << extras::red << " " << msg << "." << extras::reset;
             return ss.str();
         }
 
