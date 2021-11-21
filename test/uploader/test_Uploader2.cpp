0624230d031154630bb65c184153c57389fd9ce6
Perry Anderson <perry.anderson@gmail.com>
2021-11-21 14:14:34 -0800

    Added test case

--- test/uploader/test_Uploader2.cpp
+++ test/uploader/test_Uploader2.cpp
@@ -12,6 +12,7 @@
 #include <fstream>
 #include <filesystem>
 #include <extras/filesystem/system.hpp>
+#include <extras/status/StatusLine.hpp>
 
 #include "../unittesting/catch.hpp"
 #include "../unittesting/fakeit.hpp"
@@ -21,27 +22,64 @@ using namespace fakeit;
 using namespace std;
 namespace fs = std::filesystem;
 
+static std::string client_dir = "data/client/";
+static std::string server_dir = "data/server/";
+static std::string original = ~extras::Paths("data/exparx.webflow.zip");
+static std::string packet;
+
 class TestUploaderClient extends rsi::UploaderClient {
 public:
     virtual void connect() {};
-    virtual void transfer() const {};
+    virtual void transfer() const {
+        rsi::FileNotFoundException::assertion(filename(), __INFO__);
+        rsi::ParcelImploder parcelImploder;
+        auto wrapped = parcelImploder.wrap(filename());
+        rsi::FileNotFoundException::assertion(wrapped, __INFO__);
+        // wrapped_parcel = wrapped;
+        send(wrapped);
+        std::cout << extras::pass(wrapped) << std::endl;
+        std::cout << extras::pass("send_file2 successful") << std::endl;
+    };
     virtual void close() const {};
-    virtual void send(const rsi::Filename& filename) const {};
-    virtual void write(const rsi::Filename& filename) const {};
+    virtual void send(const rsi::Filename& filename) const {
+        auto cp_cmd = "cp " + filename + " " + server_dir;
+        SystemException::assertion(cp_cmd, __INFO__);
+    };
+    virtual void write(const rsi::Filename& filename) const {
+        auto cp_cmd = "cp " + filename + " " + client_dir;
+        SystemException::assertion(cp_cmd, __INFO__);
+    };
 };
 
 class TestUploaderServer extends rsi::UploaderServer {
 public:
     virtual void connect() {};
-    virtual void transfer() const {};
+    virtual void transfer() const {
+        rsi::ParcelImploder parcelImploder;
+        auto wrappedName = parcelImploder.wrapped(filename());
+        write(wrappedName);
+        rsi::FileNotFoundException::assertion(wrappedName, __INFO__);
+        parcelImploder.unWrap(filename());
+        parcelImploder.merge(filename());
+        auto original = parcelImploder.clean(filename());
+        auto test = fs::exists(filename());
+        auto msg = (test ? "write_file successful" : "did not upload");
+        std::cout << extras::pass(filename()) << std::endl;
+        std::cout << extras::pass(msg) << std::endl;
+    };
     virtual void close() const {};
-    virtual void send(const rsi::Filename& filename) const {};
-    virtual void write(const rsi::Filename& filename) const {};
+    virtual void send(const rsi::Filename& filename) const {
+        auto cp_cmd = "cp " + filename + " " + client_dir;
+        SystemException::assertion(cp_cmd, __INFO__);
+    };
+    virtual void write(const rsi::Filename& filename) const {
+        auto cp_cmd = "cp " + filename + " " + server_dir;
+        SystemException::assertion(cp_cmd, __INFO__);
+    };
 };
 
 SCENARIO("Test UploaderInterface: basic4", "[UploaderInterface]") {
 
-    rsi::Parameter original = ~extras::Paths("data/exparx.webflow.zip");
     rsi::Parameter _filename = original;
     rsi::Parameter _ip = "127.0.0.1";
     rsi::Parameter _port = "9500";
@@ -61,16 +99,33 @@ SCENARIO("Test UploaderInterface: basic4", "[UploaderInterface]") {
     rsi::UploaderInterface& i_client = client;
     rsi::UploaderInterface& i_server = server;
 
+    if (fs::exists(client_dir)) {
+        fs::remove_all(client_dir);
+        SystemException::assertion("mkdir " + client_dir, __INFO__);
+    }
+    if (fs::exists(server_dir)) {
+        fs::remove_all(server_dir);
+        SystemException::assertion("mkdir " + server_dir, __INFO__);
+    }
+
     REQUIRE(fs::exists(original));
+
+    auto target1 = extras::replace_all(_filename, "data/", server_dir);
+    REQUIRE(!fs::exists(target1));
+    i_client.send(_filename);
+    REQUIRE(fs::exists(target1));
+
+    auto target2 = extras::replace_all(_filename, "data/", client_dir);
+    REQUIRE(!fs::exists(target2));
+    i_client.write(_filename);
+    REQUIRE(fs::exists(target2));
+
     i_client.parameters(argc, argv1);
     i_server.parameters(argc, argv2);
 
     i_client.connect();
     i_server.connect();
-
     i_client.transfer();
-    i_client.send("filename.txt");
-    i_client.write("filename.txt");
     i_client.close();
     REQUIRE(fs::exists(original));
 
