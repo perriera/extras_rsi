063924a76bca6200c561e8f098898203b691d880
Perry Anderson <perry.anderson@gmail.com>
2021-11-21 23:59:20 -0800

    RSi-13

--- src/vendor/Server.cpp
+++ src/vendor/Server.cpp
@@ -7,6 +7,8 @@
 #include <extras/status/StatusLine.hpp>
 #include <iostream>
 #include <filesystem>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 namespace fs = std::filesystem;
@@ -42,6 +44,7 @@ namespace extras {
         std::cout << extras::blue << std::endl;
         auto fn = extras::replace_all(filename(), "data/", "data/server/");
         auto cmd = "ls -la data/server";
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
         SystemException::assertion(cmd, __INFO__);
         std::cout << extras::pass(filename()) << std::endl;
         std::cout << extras::pass(" lists directory") << std::endl;
