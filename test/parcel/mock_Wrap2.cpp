bc5a1ac3a5ea1df43fb7bfe681a30a96cb7dda8a
Perry Anderson <perry.anderson@gmail.com>
2021-11-21 21:16:23 -0800

    RSi-13

--- test/parcel/mock_Wrap2.cpp
+++ test/parcel/mock_Wrap2.cpp
@@ -47,14 +47,7 @@ SCENARIO("Mock WrapInterface: Imploder", "[WrapInterface]") {
     REQUIRE(fs::exists(imploder.original()));
     REQUIRE(!fs::exists(imploder.imploded()));
     REQUIRE(!fs::exists(imploder.exploded()));
-    cout << "HELLO JESUS" << endl;
-    cout << original << endl;
-    cout << imploded << endl;
-    cout << "HELLO JESUS" << endl;
-    auto result = i.wrap(original);
-    cout << result << endl;
-    cout << "HELLO JESUS" << endl;
-    REQUIRE(result == imploded);
+    REQUIRE(i.wrap(original) == imploded);
     REQUIRE(fs::exists(imploder.original()));
     REQUIRE(fs::exists(imploder.imploded()));
     REQUIRE(!fs::exists(imploder.exploded()));
