770e09c7f39fb8ed629a5b4943006e910fe416f4
Perry Anderson <perry.anderson@gmail.com>
2021-11-30 12:17:17 -0800

    Updarted docs

--- include/rsi/exceptions.hpp
+++ include/rsi/exceptions.hpp
@@ -1,36 +1,12 @@
 /**
- * @brief the "MIT/X Consortium License", (adapted for EXPARX.COM)
+ * @file exceptions.hpp
+ * @author Perry Anderson (perryanderson.com)
+ * @brief Adds extra keywords for C++ applications
+ * @version 1.2.1
+ * @date 2021-07-25
  *
- * Copyright (C) November 22, 2021 EXPARX INCORPORATED
- *
- * Permission is hereby  granted,  free of charge,  to  any  person
- * obtaining a copy of this software and  associated  documentation
- * files   (the  "Software"),  to deal  in   the  Software  without
- * restriction, including  without  limitation the rights  to  use,
- * copy,  modify, merge,  publish,  distribute,  sublicense, and/or
- * sell copies of the  Software, and to permit persons  to whom the
- * Software  is  furnished to  do  so,  subject  to  the  following
- * conditions:
- *
- * The above copyright notice and  this permission  notice shall be
- * included in all copies or  substantial portions of the Software.
- *
- * THE SOFTWARE IS  PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESSED  OR   IMPLIED,  INCLUDING   BUT  NOT  LIMITED  TO  THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A  PARTICULAR PURPOSE
- * AND NON-INFRINGEMENT.  IN  NO  EVENT  SHALL EXPARX  INCORPORATED
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER  IN
- * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING  FROM, OUT  OF
- * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR  OTHER DEALINGS
- * IN THE SOFTWARE.
- *
- * Except  as  contained  in this  notice, the  name of  the EXPARX
- * INCORPORATED shall not  be used in  advertising or  otherwise to
- * promote the sale, use or other dealings in this Software without
- * prior written authorization from EXPARX INCORPORATED.
- *
- * exparx.com and www.exparx.com  are domain names  registered with
- * EXPARX INCORPORATED, (other GPL-themed licenses are included).
+ * @copyright Copyright (c) 2021 Perry Anderson,
+ * All Rights Reserved, MIT License.
  *
  */
 
@@ -38,14 +14,20 @@
 #define _EXPARX_RSI_EXCEPTIONS_HPP
 
  /**
-  * @file exceptions.hpp
-  * @author Perry Anderson (perryanderson.com)
-  * @brief Adds extra keywords for C++ applications
-  * @version 1.2.1
-  * @date 2021-07-25
+  * @brief the "MIT/X Consortium License", (adapted for EXPARX.COM)
   *
-  * @copyright Copyright (c) 2021 Perry Anderson,
-  * All Rights Reserved, MIT License.
+  * Copyright (C) November 22, 2021 EXPARX INCORPORATED
+  *
+  * Permission is hereby  granted,  free of charge,  to  any  person
+  * obtaining a copy of this software and  associated  documentation
+  * files   (the  "Software"),  to deal  in   the  Software  without
+  * restriction, including  without  limitation the rights  to  use,
+  * copy,  modify, merge,  publish,  distribute,  sublicense, and/or
+  * sell copies of the  Software, and to permit persons  to whom the
+  * Software  is  furnished to  do  so,  subject  to  the  following
+  * conditions:
+  *
+  * (See LICENSE.md for complete details)
   *
   */
 
