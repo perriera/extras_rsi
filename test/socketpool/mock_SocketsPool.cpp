f1462c547f0b619dcfb32dc9192deca7b6d0f209
Perry Anderson <perry.anderson@gmail.com>
2021-12-26 22:43:19 -0800

    Added test case

--- test/socketpool/mock_SocketsPool.cpp
+++ test/socketpool/mock_SocketsPool.cpp
@@ -17,6 +17,7 @@
  */
 
 #include <extras_rsi/socketpool/SocketPool.hpp>
+#include <extras_rsi/requests/RequestType.hpp>
 #include <iostream>
 
 #include "../unittesting/catch.hpp"
@@ -27,13 +28,58 @@ using namespace fakeit;
 
 SCENARIO("Mock SocketPoolInterface", "[SocketPoolInterface]") {
 
-    Mock<rsi::SocketPoolInterface> mock;
-    When(Method(mock, transfer))
-        .AlwaysDo(
-            []() {
-            });
+    rsi::SocketParaneters parameters;
+    rsi::PortAuthority portAuthority;
+    const char* argv[] = {
+        "/home/perry/Projects/extras_rsi/build/socketpool_client",
+        "137.184.218.130",
+        "8080",
+        "data/src.zip",
+        "data/exparx.webflow.zip"
+    };
+    int argc = sizeof(argv) / sizeof(argv[0]);
+    parameters.parameters(argc, argv);
 
-    rsi::SocketPoolInterface& i = mock.get();
-    i.transfer();
-    Verify(Method(mock, transfer));
+    // Mock<rsi::LineBlockInterface> mock_lb;
+    // When(Method(mock_lb, send_line_block))
+    //     .AlwaysDo(
+    //         [](const rsi::LinePacket& msg) {
+    //         });
+    // When(Method(mock_lb, read_line_block))
+    //     .AlwaysDo(
+    //         []() {
+    //             return rsi::LinePacket();
+    //         });
+    // rsi::LineBlockInterface& ilb = mock_lb.get();
+
+    // Mock<rsi::ServiceTypeCompilerInterface> mock_stc;
+    // When(Method(mock_stc, clients))
+    //     .AlwaysDo(
+    //         [](const rsi::RequestTypeList& requests) {
+    //             return rsi::ServiceTypeList();
+    //         });
+    // rsi::ServiceTypeCompilerInterface& istc = mock_stc.get();
+
+    // Mock<rsi::SocketPoolInterface> mock;
+    // When(Method(mock, transfer))
+    //     .AlwaysDo(
+    //         [&ilb, &_socketParaneters, &istc]() {
+    //             std::string msg = _socketParaneters;
+    //             ilb.send_line_block(msg);
+    //             rsi::RequestTypeCompilation compilation(-1);
+    //             compilation.read_line_block();
+    //             auto list = compilation.compilation();
+    //             for (auto item : istc.clients(list)) {
+    //                 // cout << "msg received: " << item << endl;
+    //                 auto cmd = item;
+    //                 system(cmd.c_str());
+    //             }
+    //             // std::string cmd = "ls -la " + filename();
+    //             // extras::SystemException::assertion(cmd, __INFO__);
+    //             std::cout << std::endl;
+    //         });
+
+    // rsi::SocketPoolInterface& i = mock.get();
+    // i.transfer();
+    // Verify(Method(mock, transfer));
 }
